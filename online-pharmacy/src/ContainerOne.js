201e74c28aa4d28294b8b7441f83034594d69ec8
RL2020 <loc.fitness1@gmail.com>
2022-06-29 20:13:14 -0400

    cleaned file tree, Containerone Component

---
+++ online-pharmacy/src/ContainerOne.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+ const ContainerOne = () => {
+    
+        return (
+
+            <div>      
+            <h1> Online Pharmacy</h1>
+            <p> Welcome to the online Pharmacy!</p>
+            <p>Where you can browse and purchase your medications online </p>     
+            <p>from the comfort of your own home</p> 
+            </div>
+        
+
+        );
+    
+}
+
+export default ContainerOne;
